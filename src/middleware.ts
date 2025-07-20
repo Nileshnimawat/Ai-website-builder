8161400941ee00346adfb2a334af49d00588fbe2
NILESH <nileshnimawat121@gmail.com>
2025-07-20 19:39:09 +0530

    prima prodcution setup

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
+// /src/middleware.ts
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)','/sign-up(.*)', '/'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)','/sign-up(.*)', '/', '/favicon.ico'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
@@ -9,10 +10,5 @@ export default clerkMiddleware(async (auth, req) => {
 })
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"], // Official matcher
 };
